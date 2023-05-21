c1b8d277df81ed053310f7faa0e19a3dfe4ed6d1
wunphatboi <oliver.lowman@student.adelaide.edu.au>
2023-05-21 22:37:27 +0930

    code

--- klargest.cpp
+++ klargest.cpp
@@ -2,18 +2,12 @@
 #include <queue>
 
 int kth_largest(std::vector<int> values, int k) {
-    // Create a max heap using priority_queue
     std::priority_queue<int> maxHeap;
-
-    // Insert elements from the vector into the max heap
     for (int value : values) {
         maxHeap.push(value);
     }
-
-    // Extract the kth largest element from the max heap
     for (int i = 1; i < k; i++) {
         maxHeap.pop();
     }
-
     return maxHeap.top();
 }
